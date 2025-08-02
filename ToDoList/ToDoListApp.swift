3aa1bcedb3896ad0e90a65c242d95cc48630faa3
ZYP <zayarphyo16.mm@gmail.com>
2025-08-02 17:04:32 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Zay Yar Phyo on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
